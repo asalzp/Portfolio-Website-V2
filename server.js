5c8b46bc70d82eebdaa5f20b35f542c4fad10a00
asalzp <azarepakziab@mun.ca>
2024-05-17 21:12:13 -0230

    adding server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
